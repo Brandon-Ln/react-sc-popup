a2ca5c98a105cc21e1830dd067f77c376411ddc9
Brandon Leon <awesome163@126.com>
2026-05-16 17:36:12 +0800

    chore(package.json): simplify prepare script and update lockfile version

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
 import './styles/index';
 export { Popup } from './Popup';
 export { config } from '@react-spring/web';
-export * from './interface';
+
+export type {
+  PopupInstance,
+  PopupPlacement,
+  PopupProps,
+  UncontrolledPopupProps,
+} from './interface';
+export type { SpringConfig, SpringProps } from '@react-spring/web';
